4fdde11e59173d35188f551d661519d797e78c4b
Chandanagayathrig2003 <97158494+Chandanagayathrig2003@users.noreply.github.com>
2026-05-24 20:09:26 +0530

    Update app.js

--- cicd-monitor-dashboard/backend/src/app.js
+++ cicd-monitor-dashboard/backend/src/app.js
@@ -2,19 +2,30 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+
 import deploymentRoutes from './routes/deploymentRoutes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*'
+  })
+);
+
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Health API
 app.get('/api/health', (req, res) => {
-  res.json({ status: 'OK' });
+  res.json({
+    status: 'OK',
+    uptime: process.uptime()
+  });
 });
 
+// Deployment APIs
 app.use('/api/deployments', deploymentRoutes);
 
 export default app;
