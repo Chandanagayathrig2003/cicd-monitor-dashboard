cc695ed7c5ca286eb90c95fb16d6f17beeab9b40
Chandanagayathrig2003 <97158494+Chandanagayathrig2003@users.noreply.github.com>
2026-05-24 21:47:54 +0530

    Create Deployments.jsx

---
+++ cicd-monitor-dashboard/frontend/src/pages/Deployments.jsx
@@ -0,0 +1,22 @@
+import Sidebar from '../components/layout/Sidebar';
+
+function Deployments() {
+  return (
+    <div className="flex bg-[#020617] min-h-screen text-white">
+
+      <Sidebar />
+
+      <div className="flex-1 p-10">
+        <h1 className="text-5xl font-bold">
+          Deployments
+        </h1>
+
+        <p className="text-gray-400 mt-4">
+          Monitor all deployment pipelines and environments.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Deployments;
